6cdec9c0f36c60d8cdd82f7f3a0478abf022d344
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-03-14 16:26:33 +0300

    create files for constants

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:e_commerce_app/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme:TAppTheme.lightTheme,
+      darkTheme: TAppTheme.darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
+
   runApp(const ECommerceApp());
-}
-
-class ECommerceApp extends StatelessWidget {
-  const ECommerceApp ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme:TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
