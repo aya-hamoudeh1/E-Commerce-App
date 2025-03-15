9ee29a02895773319876bbac5c71e38d7b548921
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-03-16 00:18:27 +0300

    OnBoarding screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,18 @@
+import 'package:e_commerce_app/features/authentication/screens/onbaoarding/onboarding.dart';
 import 'package:e_commerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ECommerceApp extends StatelessWidget {
-  const ECommerceApp ({super.key});
+  const ECommerceApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
-      theme:TAppTheme.lightTheme,
+      theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -6,4 +6,9 @@ class TImages{
   // Social Logos
   static const String google = "";
   static const String facebook = "";
+
+  // OnBoarding Images
+static const String onBoardingImage1 = "assets/images/on_boarding_images/test.svg";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/test.svg";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/test.svg";
 }
