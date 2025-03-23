b2e14aec7aff13aee532b1d1968f35d52fa59a83
aya-hamoudeh1 <ayahamoudeh29@gmail.com>
2025-03-23 03:16:39 +0300

    Shopping Cart Screen

--- lib/common/widgets/products/cart/cart_menu_icon.dart
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -1,4 +1,7 @@
+import 'package:e_commerce_app/features/shop/screens/cart/cart.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import '../../../../utils/constants/colors.dart';
 
@@ -7,17 +10,20 @@ class TCartCounterIcon extends StatelessWidget {
     super.key,
     required this.onPressed,
     this.iconColor,
+    this.counterBgColor,
+    this.counterTextColor,
   });
 
   final VoidCallback onPressed;
-  final Color? iconColor;
+  final Color? iconColor, counterBgColor, counterTextColor;
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunction.isDarkMode(context);
     return Stack(
       children: [
         IconButton(
-          onPressed: onPressed,
+          onPressed: () => Get.to(() => const CartScreen()),
           icon: Icon(
             Iconsax.shopping_bag,
             color: iconColor,
@@ -29,16 +35,16 @@ class TCartCounterIcon extends StatelessWidget {
             height: 18,
             width: 18,
             decoration: BoxDecoration(
-              color: TColors.black.withOpacity(0.5),
+              color: counterBgColor ??(dark ? TColors.white : TColors.black),
               borderRadius: BorderRadius.circular(100),
             ),
             child: Center(
               child: Text(
                 "2",
                 style: Theme.of(context).textTheme.labelLarge!.apply(
-                  color: TColors.white,
-                  fontSizeFactor: 0.8,
-                ),
+                      color: TColors.white,
+                      fontSizeFactor: 0.8,
+                    ),
               ),
             ),
           ),
